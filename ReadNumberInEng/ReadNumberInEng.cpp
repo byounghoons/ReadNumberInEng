e76d10f1c5ce260c48f25c63a1b81e10e97d5090
bhshin <byounghoons@hotmail.com>
2016-08-07 12:50:00 +0900

    add tests

--- ReadNumberInEng/ReadNumberInEng.cpp
+++ ReadNumberInEng/ReadNumberInEng.cpp
@@ -10,8 +10,7 @@ using namespace std;
 
 static const long long RANGE_BILLION = 1000000000;
 
-static const string table_units[] = { "billions", "millions", "thousands", "" };
-static const string table_units_one[] = { "billion", "million", "thousand", "" };
+static const string table_units[] = { "billion", "million", "thousand", "" };
 
 static const string table_single_digits[] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 static const string table_ten_to_twenty[] = { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
@@ -30,7 +29,9 @@ string print_number_lessthan_thousand(int number_lessthan_thousand)
 	if (hundreds > 0)
 	{
 		str_hundreds = table_single_digits[hundreds - 1];
-		result = str_hundreds + " " + (hundreds == 1 ? "hundred" : "hundreds");
+		result = str_hundreds + " hundred";
+		if (hundreds > 1)
+			result += "s";
 	}
 
 	int tens = number_lessthan_thousand % 100;
@@ -59,26 +60,9 @@ string print_number_lessthan_thousand(int number_lessthan_thousand)
 	return result;
 }
 
-int main()
+string print_number_in_english(long long input, long long max_range)
 {
-	long long input = 0;
-	long long max_range = RANGE_BILLION; // billion
-
-	cout << "Please input numbers to read (max is" << max_range * 1000 - 1 << ")\n";
-	cin >> input;
-	if (cin.fail())
-	{
-		cout << "Error happens\n";
-		return 0;
-	}
-
-	if (input >= max_range * 1000)
-	{
-		cout << "It's too big number. Sorry we can't handle it\n";
-		return 0;
-	}
-
-	string result;
+	string result = "";
 	int idx_unit = 0;
 	long long source = input;
 
@@ -88,15 +72,93 @@ int main()
 		if (div > 0)
 		{
 			result += print_number_lessthan_thousand(div);
-			result += " ";
-			result += div == 1 ? table_units_one[idx_unit] : table_units[idx_unit];
-			result += " ";
+			
+			string unit = table_units[idx_unit];
+			if (unit != "")
+			{ 
+				result += " " + unit;
+				if (div > 1)
+					result += "s";
+			}
 		}
+
 		source = source % max_range;
 		max_range = max_range / 1000;
 		idx_unit++;
+
+		if (div > 0 && max_range > 0)
+			result += " ";
 	}
 
+	return result;
+}
+
+typedef struct 
+{ 
+	long long number; 
+	string reading;
+} NUMBER_READING;
+
+// tests
+
+static const NUMBER_READING test_data[] =
+{
+	{1, "one"},
+	{15, "fifteen"},
+	{1001, "one thousand one"},
+	{2311116, "two millions three hundreds eleven thousands one hundred sixteen"}
+};
+
+bool run_tests(long long max_input)
+{
+	bool is_fail = false;
+
+	cout << "test mode\n";
+	for (int i = 0; i < sizeof(test_data) / sizeof(NUMBER_READING); i++)
+	{
+		string result = print_number_in_english(test_data[i].number, max_input);
+		if (result.compare(test_data[i].reading) != 0)
+		{
+			is_fail = true;
+
+			cout << "error:";
+			cout << test_data[i].number << ",";
+			cout << test_data[i].reading;
+			cout << "(result:" << result;
+			cout << ")\n";
+		}
+	}
+	
+	return is_fail;
+}
+
+int main(int argc, char *argv[])
+{
+	long long input = 0;
+	long long MAX_INPUT = RANGE_BILLION; // billion
+
+	// test mode
+	if (argc > 1 && strcmp(argv[1], "test") == 0)
+	{
+		run_tests(MAX_INPUT);
+		return 0;
+	}
+
+	cout << "Please input numbers to read (max is" << MAX_INPUT * 1000 - 1 << ")\n";
+	cin >> input;
+	if (cin.fail())
+	{
+		cout << "Error happens\n";
+		return 0;
+	}
+
+	if (input >= MAX_INPUT * 1000)
+	{
+		cout << "It's too big number. Sorry we can't handle it\n";
+		return 0;
+	}
+
+	string result = print_number_in_english(input, MAX_INPUT);
 	cout << result << "\n";
 
 	return 0;
